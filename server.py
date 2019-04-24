0f1e4d7934f481e7254d30dff1c90b0ff0cb18da
Mattia <mattiafailla@hotmail.it>
2019-04-24 22:18:57 +0200

    Adapting server versioning

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-import SocketServer
+#!/usr/bin/python3
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
     """
     It is instantiated once per connection to the server, and must
     override the handle() method to implement communication to the
@@ -10,8 +12,8 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
+        print ("{} wrote:".format(self.client_address[0]))
+        print (self.data)
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
 
@@ -19,7 +21,7 @@ if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
 
     # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
