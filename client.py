f7bf6cc5d1d69144d9dbbaf70f08d595e5e92015
Mattia <mattiafailla@hotmail.it>
2019-04-25 00:43:24 +0200

    Adding TODOs
    Adding todos and defining the SlaveHandler class

--- client.py
+++ client.py
@@ -1,23 +1,37 @@
 import socket
 import config.configured_logger
 
+# TODO:
+"""
++ Implement headers
++ Implement an ID system
++ Implement a send_forever directive
++ Implement an intent broker
++ Implement a master-watchdogs
+"""
 logger = config.configured_logger.logger
 
 
-hostname, port = 'localhost', 9999
-target = hostname
+class SlaveHandler(object):
+    """docstring for SlaveHandler."""
+    def __init__(self, arg):
+        super(SlaveHandler, self).__init__()
+        self.arg = arg
 
-# create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect the client
-# client.connect((target, port))
-client.connect((target, port))
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
 
-# send some data (in this case a HTTP GET request)
-client.send('hi'.encode('utf-8'))
+    # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# receive the response data (4096 is recommended buffer size)
-response = client.recv(4096)
+    # connect the client
+    client.connect((HOST, PORT))
 
-logger.debug (response)
+    # send some data
+    client.send('dummy data'.encode('utf-8'))
+
+    # receive the response data (4096 is recommended buffer size for incoming commands)
+    response = client.recv(4096)
+
+    logger.debug (response)
