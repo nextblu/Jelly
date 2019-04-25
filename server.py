631156c9d9cc8d77b82c9da024d82a2fe647f372
Mattia <mattiafailla@hotmail.it>
2019-04-25 15:24:37 +0200

    Implementing service announcement

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
 import socketserver
+from time import sleep
+from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST, gethostbyname, gethostname
 import config.configured_logger
 
 logger = config.configured_logger.logger
+
+def ServiceAnnouncement(HOST, PORT, MAGIC):
+    s = socket(AF_INET, SOCK_DGRAM) #create UDP socket
+    s.bind(('', 0))
+    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #this is a broadcast socket
+    my_ip= gethostbyname(gethostname()) #get our IP. Be careful if you have multiple network interfaces or IPs
+
+    while 1:
+        data = MAGIC+my_ip
+        s.sendto(data, ('<broadcast>', PORT))
+        print "sent service announcement"
+        sleep(5)
+
 
 class TCPHandler(socketserver.BaseRequestHandler):
     """
@@ -23,7 +38,7 @@ class TCPHandler(socketserver.BaseRequestHandler):
         pass
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT, MAGIC = "localhost", 9999, "JellySERVER"
 
     # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), TCPHandler)
